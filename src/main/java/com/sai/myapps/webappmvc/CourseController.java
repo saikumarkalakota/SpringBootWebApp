59e7238763d3537191aa272fcaa0729b5d136003
saikumarkalakota <58147758+saikumarkalakota@users.noreply.github.com>
2020-06-28 00:45:05 +0530

    Spring boot based Web app returning JSP page

---
+++ src/main/java/com/sai/myapps/webappmvc/CourseController.java
@@ -0,0 +1,21 @@
+package com.sai.myapps.webappmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Controller
+
+public class CourseController {
+	@RequestMapping("/courses")
+	public String courses() {
+		
+		return "courses";
+	}
+	
+	
+	
+
+}
---
+++ src/main/webapp/webpages/courses.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>My Courses Web Page</title>
+</head>
+<body>
+<h1>Here's the list of courses</h1> <br>
+1. Java<br>
+2. J2EE<br>
+3. Spring<br>
+4. Spring boot<br>
+</body>
+</html>
