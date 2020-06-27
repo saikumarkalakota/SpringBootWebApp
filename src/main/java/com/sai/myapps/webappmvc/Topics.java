8d51bced100837711a4b9966a061e3be0b7baf7d
saikumarkalakota <58147758+saikumarkalakota@users.noreply.github.com>
2020-06-28 01:08:50 +0530

    Added Topics class (Bean) to demo IoC principle implementation via DI

---
+++ src/main/java/com/sai/myapps/webappmvc/Topics.java
@@ -0,0 +1,13 @@
+package com.sai.myapps.webappmvc;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Topics {
+	
+	public void display()
+	{
+		System.out.println("This is a Topics Bean");
+	}
+
+}
--- src/main/java/com/sai/myapps/webappmvc/WebappmvcApplication.java
+++ src/main/java/com/sai/myapps/webappmvc/WebappmvcApplication.java
@@ -2,12 +2,17 @@ package com.sai.myapps.webappmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WebappmvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebappmvcApplication.class, args);
+		ConfigurableApplicationContext con = SpringApplication.run(WebappmvcApplication.class, args);
+		Topics top= con.getBean(Topics.class);
+		top.display();
+		
+	
 	}
 
 }
